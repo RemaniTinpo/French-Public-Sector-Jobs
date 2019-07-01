10241097839b4d75f824a16e43c648132d128db3
marien <monsieurpinot06@gmail.com>
2019-07-01 15:41:57 +0200

    Incorrect path

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src.scrapper import scrapper
 
 sc = scrapper()
 db = sc.collect()
-db.to_csv('/data/place_emploi_public.csv', index=False)
+db.to_csv('data/place_emploi_public.csv', index=False)
